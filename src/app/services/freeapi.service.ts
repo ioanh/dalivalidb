38f45fd34fcbdb769b91981ca74e401f999f47f3
ioanh <ioan.xristov@abv.bg>
2021-04-08 23:11:33 +0300

    pointingtheAPI to Dolna banya

--- src/app/services/freeapi.service.ts
+++ src/app/services/freeapi.service.ts
@@ -9,7 +9,7 @@ export class api {
     constructor(private httpclinet: HttpClient) {}
     getapi(): Observable<any>{ 
         
-        return this.httpclinet.get(`http://api.openweathermap.org/data/2.5/weather?q=London,%20Paris&appid=70c110dbd331e0f14f9cd29f78c32ff0`)
+        return this.httpclinet.get(`http://api.openweathermap.org/data/2.5/weather?q=Dolna%20Banya&appid=70c110dbd331e0f14f9cd29f78c32ff0`)
     }
     
 }
