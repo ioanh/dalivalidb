4aedc120b84b1f09dc99a2ee757a1bc976e17497
ioanh <ioan.xristov@abv.bg>
2021-06-11 01:00:43 +0300

    adding more data for the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,10 @@ export class AppComponent {
   currentWeather: number;
   typeOfWeather: string;
   imagePath: string;
+  cityName: string;
+  lon: number;
+  lat: number;
+  description: string;
 
   ngOnInit(){
     this._api.getapi()
@@ -26,11 +30,28 @@ export class AppComponent {
       data=>{
         //FETCHING THE DATA 
         this.LondonWeather = data;
+
         // TRANSFORMING THE DEGREES TO C and ROUNDING THEM 
         this.currentWeather = Math.round(this.LondonWeather.main.feels_like - 273.15);
         this.typeOfWeather = this.LondonWeather.weather[0].main
+
         //ADDING THE IMAGE PATH
-        this.imagePath = `../assets/${this.typeOfWeather}.png`;
+        if(this.typeOfWeather == 'Mist' || this.typeOfWeather == 'Smoke' || this.typeOfWeather == 'Haze'|| this.typeOfWeather == 'Dust'|| this.typeOfWeather == 'Fog'|| this.typeOfWeather == 'Sand'|| this.typeOfWeather == 'Dust'|| this.typeOfWeather == 'Ash'|| this.typeOfWeather == 'Squall'|| this.typeOfWeather == 'Tornado' ){
+          this.imagePath = `../assets/Snow.png`;
+        }else{
+          this.imagePath = `../assets/${this.typeOfWeather}.png`;
+        }
+
+        //GETTING THE CITY NAME 
+        this.cityName = this.LondonWeather.name
+
+        // GETTING THE COORDINATES
+        this.lon = this.LondonWeather.coord.lon
+        this.lat = this.LondonWeather.coord.lat
+
+        //GETTING THE DESCRIPTION
+        this.description = this.LondonWeather.weather[0].description
+
         //CW THE MODEL
         console.log(this.LondonWeather)
       }
