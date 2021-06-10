f201eadf1b061390d9d7b71001e898b9c3942b94
ioanh <ioan.xristov@abv.bg>
2021-06-11 01:30:46 +0300

    making a dynamic API call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,31 +30,56 @@ export class AppComponent {
       data=>{
         //FETCHING THE DATA 
         this.LondonWeather = data;
-
-        // TRANSFORMING THE DEGREES TO C and ROUNDING THEM 
-        this.currentWeather = Math.round(this.LondonWeather.main.feels_like - 273.15);
-        this.typeOfWeather = this.LondonWeather.weather[0].main
-
-        //ADDING THE IMAGE PATH
-        if(this.typeOfWeather == 'Mist' || this.typeOfWeather == 'Smoke' || this.typeOfWeather == 'Haze'|| this.typeOfWeather == 'Dust'|| this.typeOfWeather == 'Fog'|| this.typeOfWeather == 'Sand'|| this.typeOfWeather == 'Dust'|| this.typeOfWeather == 'Ash'|| this.typeOfWeather == 'Squall'|| this.typeOfWeather == 'Tornado' ){
-          this.imagePath = `../assets/Snow.png`;
-        }else{
-          this.imagePath = `../assets/${this.typeOfWeather}.png`;
-        }
-
-        //GETTING THE CITY NAME 
-        this.cityName = this.LondonWeather.name
-
-        // GETTING THE COORDINATES
-        this.lon = this.LondonWeather.coord.lon
-        this.lat = this.LondonWeather.coord.lat
-
-        //GETTING THE DESCRIPTION
-        this.description = this.LondonWeather.weather[0].description
-
-        //CW THE MODEL
-        console.log(this.LondonWeather)
+        this.updatingFrontend();
       }
     );
+
+    //LISTENING TO QUERY CHANGE 
+    this._api.queryChanged.subscribe(
+      (query) =>{
+        this._api.getapi(query).subscribe(
+          data => {
+            this.LondonWeather = data;
+            this.updatingFrontend();
+          }
+        )
+      }
+    )
   }  
+
+
+
+
+
+  //GETTING THE QUERY FROM USER AND EMITTING THE DATA
+  cityWasSelected(query){
+    this._api.queryChanged.emit(query)
+  }
+
+
+  updatingFrontend(){
+    // TRANSFORMING THE DEGREES TO C and ROUNDING THEM 
+    this.currentWeather = Math.round(this.LondonWeather.main.feels_like - 273.15);
+    this.typeOfWeather = this.LondonWeather.weather[0].main
+  
+    //ADDING THE IMAGE PATH
+    if(this.typeOfWeather == 'Mist' || this.typeOfWeather == 'Smoke' || this.typeOfWeather == 'Haze'|| this.typeOfWeather == 'Dust'|| this.typeOfWeather == 'Fog'|| this.typeOfWeather == 'Sand'|| this.typeOfWeather == 'Dust'|| this.typeOfWeather == 'Ash'|| this.typeOfWeather == 'Squall'|| this.typeOfWeather == 'Tornado' ){
+      this.imagePath = `../assets/Snow.png`;
+    }else{
+      this.imagePath = `../assets/${this.typeOfWeather}.png`;
+    }
+  
+    //GETTING THE CITY NAME 
+    this.cityName = this.LondonWeather.name
+  
+    // GETTING THE COORDINATES
+    this.lon = this.LondonWeather.coord.lon
+    this.lat = this.LondonWeather.coord.lat
+  
+    //GETTING THE DESCRIPTION
+    this.description = this.LondonWeather.weather[0].description
+  
+    //CW THE MODEL
+    console.log(this.LondonWeather)
+  }
 }
--- src/app/services/freeapi.service.ts
+++ src/app/services/freeapi.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
@@ -7,9 +8,10 @@ import { catchError, retry } from 'rxjs/operators';
 @Injectable()
 export class api {
     constructor(private httpclinet: HttpClient) {}
-    getapi(): Observable<any>{ 
+    queryChanged = new EventEmitter<string>();
+    getapi(query: string = 'Dolna%20Banya'): Observable<any>{ 
         
-        return this.httpclinet.get(`http://api.openweathermap.org/data/2.5/weather?q=Dolna%20Banya&appid=70c110dbd331e0f14f9cd29f78c32ff0`)
+        return this.httpclinet.get(`http://api.openweathermap.org/data/2.5/weather?q=${query}&appid=70c110dbd331e0f14f9cd29f78c32ff0`)
     }
     
 }
