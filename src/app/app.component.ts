7a2e63dea87b57ca4462a381cd6be371faf197dc
ioanh <ioan.xristov@abv.bg>
2021-06-11 18:21:53 +0300

    removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import{weatherAPImodel} from './services/weatherAPImodel'
-import {api} from './services/freeapi.service'
 
 @Component({
   selector: 'app-root',
