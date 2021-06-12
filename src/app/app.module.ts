9975ef6b92437e91400b8be68179f71df93bd9f4
ioanh <ioan.xristov@abv.bg>
2021-06-12 13:18:36 +0300

    creating the header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import {api} from './services/freeapi.service';
 import { SearchDirective } from './search.directive';
 import { WeatherComponent } from './weather/weather.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchDirective,
-    WeatherComponent
+    WeatherComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
