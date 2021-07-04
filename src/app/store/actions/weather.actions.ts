e00aa41caa7fb4b23b1b3d675c7abc2e4025d56d
ioanh <ioan.xristov@abv.bg>
2021-07-05 00:30:18 +0300

    installing ngrx store and finalizing the actions

--- src/app/store/actions/weather.actions.ts
+++ src/app/store/actions/weather.actions.ts
@@ -1,5 +1,28 @@
+import {Action} from '@ngrx/store'
+import {weatherAPImodel} from '../../services/weatherAPImodel'
+
 export enum weatherActionTypes {
-    GET_WEATHER = 'GET WEATHER ACTION',
-    GET_WEATHER_SUCESS = 'GET WEATHER SUCCESS ACTION',
-    GET_WEATHER_FAILURE = 'GET WEATHER FAILURE ACTION'
+    GET_WEATHER = '[WEATHER] Get Weather Action',
+    GET_WEATHER_SUCESS = '[WEATHER] Get Weather Success Action',
+    GET_WEATHER_FAILURE = '[WEATHER] Get Weather Failure Action'
 }
+
+export class GetWeatherAction implements Action {
+    readonly type = weatherActionTypes.GET_WEATHER
+
+    constructor(private payload: string) {}
+}
+
+export class GetWeatherSuccessAction implements Action {
+    readonly type = weatherActionTypes.GET_WEATHER_SUCESS
+
+    constructor(private payload: weatherAPImodel[]) {}
+}
+
+export class GetWeatherFailureAction implements Action {
+    readonly type = weatherActionTypes.GET_WEATHER_FAILURE
+
+    constructor(private payload: Error){}
+}
+
+export type WeatherAction = GetWeatherAction | GetWeatherSuccessAction | GetWeatherFailureAction 
